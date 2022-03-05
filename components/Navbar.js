a9edfb4550f0228df485a208c62a106ee800731f
burketrevor <trevor@dtschurch.com>
2022-03-04 19:16:22 -0500

    Adding Navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,50 @@
-import styles from "../styles/Home.module.scss";
+import React, { useState } from "react"
+import styles from "../styles/Home.module.scss"
+import { RiMenu4Fill } from "react-icons/ri"
+import { AiOutlineCloseSquare } from "react-icons/ai"
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const menuToggler = () => setMenuOpen((p) => !p)
+
   return (
     <div className={styles.header}>
-      <div id="{styles.brand}">
-        <a href="/">Hexonix Studios</a>
+      <div className={styles.header__content}>
+        <div>
+          <span className={styles.logo}>
+            <img src="./hexonic-icon-white.png"></img>
+          </span>
+        </div>
+        <div>
+          <nav
+            className={`${styles.nav} ${menuOpen ? styles[`nav--open`] : {}}`}
+          >
+            <a className={styles.nav__item} href={"/"}>
+              About
+            </a>
+            <a className={styles.nav__item} href={"/"}>
+              Our Work
+            </a>
+            <div className={styles.nav__button__container}>
+              <Button />
+            </div>
+          </nav>
+        </div>
+        <div>
+          <div className={styles.header__button__container}>
+            <Button />
+          </div>
+          <button className={styles.header__toggler} onClick={menuToggler}>
+            <RiMenu4Fill />
+          </button>
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+const Button = () => {
+  return <button className={styles.button}>Contact</button>
+}
+
+export default Navbar
