d0da13d35fa3617fbcbe20c6b7c3dd9533f0ef69
burketrevor <trevor@dtschurch.com>
2022-03-15 03:55:51 -0400

    remove unused image code

--- pages/index.js
+++ pages/index.js
@@ -38,32 +38,6 @@ export default function Home({ images }) {
             <span></span>
           </div>
         </section>
-        {/*}
-        <section>
-          <h1 className="sr-only">My Images</h1>
-
-          <h2 className={styles.header}>Images</h2>
-
-          <ul className={styles.images}>
-            {images.map((image) => {
-              return (
-                <li key={image.id}>
-                  <a href={image.link} rel="noreferrer">
-                    <div className={styles.imageImage}>
-                      <Image
-                        width={image.width}
-                        height={image.height}
-                        src={image.image}
-                        alt=""
-                      />
-                    </div>
-                  </a>
-                </li>
-              )
-            })}
-          </ul>
-        </section>
-{*/}
       </Container>
       {/*}About Section{*/}
       <section>
@@ -160,36 +134,4 @@ export default function Home({ images }) {
       </section>
     </Layout>
   )
-}
-
-export async function getStaticProps() {
-  const results = await fetch(
-    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image/tags/Lava`,
-    {
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          process.env.CLOUDINARY_API_KEY +
-            ":" +
-            process.env.CLOUDINARY_API_SECRET
-        ).toString("base64")}`,
-      },
-    }
-  ).then((r) => r.json())
-  const { resources } = results
-  const images = resources.map((resource) => {
-    const { width, height } = resource
-    return {
-      id: resource.asset_id,
-      title: resource.public_id,
-      image: resource.secure_url,
-      width,
-      height,
-    }
-  })
-
-  return {
-    props: {
-      images,
-    },
-  }
 }
