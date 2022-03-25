832daa4eec7166d79d8e771e199d590b80b7eb2a
Trevor Burke <burke.trevor.m@gmail.com>
2022-03-25 11:56:26 -0400

    Adding images and text to About page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,15 @@
-import Head from "next/head"
-import Image from "next/image"
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import HoverImage from "react-hover-image";
 
-import Layout from "../components/Layout"
-import Container from "../components/Container"
-import styles from "../styles/Home.module.scss"
+import Layout from "../components/Layout";
+import Container from "../components/Container";
+
+import redTim from "../public/tim-red.jpg";
+import normalTim from "../public/tim-normal.jpg";
+
+import styles from "../styles/Home.module.scss";
 
 export default function About() {
   return (
@@ -15,10 +21,131 @@ export default function About() {
           content="A Collaborative Space for Creative Excellence."
         />
       </Head>
-      
-    <div className={styles.main}>
-      <h1>About</h1>
-    </div>
+      <Container>
+        <div className={styles.main}>
+          <h1>About</h1>
+          <p>
+            Videography is capturing a story
+            within an environment. Cinematography
+            is capturing that story while creating
+            the environment. We are experts in
+            both.
+          </p>
+          <p>
+            Excellence in video, it’s all we know.
+            Our model is simple: whether it’s
+            vertical reels or full-scaled
+            productions, we bring our best. We’ve
+            been creating compelling videos for
+            over a decade. Some of us are
+            life-long entrepreneurs and others
+            have crossed over from corporate jobs.
+          </p>
+          <p>
+            About four years ago, TJ Cardinal
+            contracted Tim Doerfler to shoot a
+            commercial for one of his companies.
+            They produced a series of
+            made-for-social videos that took TJ’s
+            business to the next level. Together,
+            they invested in gear and a studio
+            space. Other companies started to
+            notice the compelling and interesting
+            videos that were coming out of what is
+            now Hexonic Studios. The success
+            brought in clients like
+            Sherwin-Williams, Wal-Mart, and
+            NASCAR. Tyler and Cole were added to
+            the team to not only help with the
+            workload, but to add new perspectives
+            and experience.
+          </p>
+          <p>
+            In 2019 we recognized that we had
+            something special and decided to
+            become intentional about offering our
+            services to the world.
+          </p>
+        </div>
+      </Container>
+      <Container>
+        <h1>The Team</h1>
+        <div className={styles.team}>
+          {/*}Tim{*/}
+          <div className={styles.team__member}>
+            <HoverImage
+              className={styles.team__photo}
+              src={
+                "https://res.cloudinary.com/burke-creative/image/upload/v1648146128/Hexonic%20Studios/Team%20Photos/5X8A8759_njobci.jpg"
+              }
+              hoverSrc={
+                "https://res.cloudinary.com/burke-creative/image/upload/v1648146129/Hexonic%20Studios/Team%20Photos/5X8A8791-Edit-Edit_lc9pfn.jpg"
+              }
+            />
+            <h2 className={styles.team__name}>
+              Tim Doerfler
+            </h2>
+            <h4 className={styles.team__title}>
+              Production Lead/Director of
+              Cinematography
+            </h4>
+          </div>
+          {/*}TJ{*/}
+          <div className={styles.team__member}>
+            <HoverImage
+              className={styles.team__photo}
+              src={
+                "https://res.cloudinary.com/burke-creative/image/upload/v1648146127/Hexonic%20Studios/Team%20Photos/5X8A8749_nsblfb.jpg"
+              }
+              hoverSrc={
+                "https://res.cloudinary.com/burke-creative/image/upload/v1648146129/Hexonic%20Studios/Team%20Photos/5X8A8784-Edit_kjxqbg.jpg"
+              }
+            />
+            <h2 className={styles.team__name}>
+              TJ Cardinal
+            </h2>
+            <h4 className={styles.team__title}>
+              Creative Director/Writer
+            </h4>
+          </div>
+          {/*}Cole{*/}
+          <div className={styles.team__member}>
+            <HoverImage
+              className={styles.team__photo}
+              src={
+                "https://res.cloudinary.com/burke-creative/image/upload/v1648146128/Hexonic%20Studios/Team%20Photos/5X8A8750_l9za4f.jpg"
+              }
+              hoverSrc={
+                "https://res.cloudinary.com/burke-creative/image/upload/v1648146129/Hexonic%20Studios/Team%20Photos/5X8A8788-Edit_st5gu3.jpg"
+              }
+            />
+            <h2 className={styles.team__name}>
+              Cole Sarver
+            </h2>
+            <h4 className={styles.team__title}>
+              Short Format Director/Animation
+            </h4>
+          </div>
+          {/*}Tyler{*/}
+          <div className={styles.team__member}>
+            <HoverImage
+              className={styles.team__photo}
+              src={
+                "https://res.cloudinary.com/burke-creative/image/upload/v1648146129/Hexonic%20Studios/Team%20Photos/5X8A8754_ohadsf.jpg"
+              }
+              hoverSrc={
+                "https://res.cloudinary.com/burke-creative/image/upload/v1648146128/Hexonic%20Studios/Team%20Photos/5X8A8779-Edit_awq5rj.jpg"
+              }
+            />
+            <h2 className={styles.team__name}>
+              Tyler Neading
+            </h2>
+            <h4 className={styles.team__title}>
+              Videography Lead
+            </h4>
+          </div>
+        </div>
+      </Container>
     </Layout>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
-import { useState, useEffect } from "react"
-import Head from "next/head"
-import Image from "next/image"
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
 
-import Layout from "../components/Layout"
-import Container from "../components/Container"
+import Layout from "../components/Layout";
+import Container from "../components/Container";
 
-import images from "../data/images.json"
+import images from "../data/images.json";
 
-import hero from "../public/hexoniclogowide.png"
+import hero from "../public/hexoniclogowide.png";
 
-import styles from "../styles/Home.module.scss"
-import { ParallaxBanner, ParallaxProvider } from "react-scroll-parallax"
+import styles from "../styles/Home.module.scss";
+import {
+  ParallaxBanner,
+  ParallaxProvider,
+} from "react-scroll-parallax";
 
 export default function Home({ images }) {
   return (
@@ -23,18 +26,44 @@ export default function Home({ images }) {
         />
       </Head>
       <Container>
-        <section id="intro" className={styles.main}>
+        <section
+          id="intro"
+          className={styles.main}>
           <h1 className={styles.title}>
             Welcome to Hexonic Studios. <br />
             <span>We make videos.</span>
           </h1>
-          
-          <div className={styles.scrolldown__wrapper}>
+
+          <div
+            className={
+              styles.scrolldown__wrapper
+            }>
             <div className={styles.scrolldown}>
               <svg height="300" width="20">
-                <circle className={styles.scrolldown__p1} cx="5" cy="15" r="3" />
-                <circle className={styles.scrolldown__p2} cx="5" cy="15" r="3" />                 
-                <circle className={styles.scrolldown__p3} cx="5" cy="15" r="3" />                 
+                <circle
+                  className={
+                    styles.scrolldown__p1
+                  }
+                  cx="5"
+                  cy="15"
+                  r="3"
+                />
+                <circle
+                  className={
+                    styles.scrolldown__p2
+                  }
+                  cx="5"
+                  cy="15"
+                  r="3"
+                />
+                <circle
+                  className={
+                    styles.scrolldown__p3
+                  }
+                  cx="5"
+                  cy="15"
+                  r="3"
+                />
               </svg>
             </div>
           </div>
@@ -62,20 +91,33 @@ export default function Home({ images }) {
                     />
                   ),
                 },
-              ]}
-            ></ParallaxBanner>
+              ]}></ParallaxBanner>
           </ParallaxProvider>
           <Container>
             <div className={styles.about}>
-              <div className={styles.about__title}>
-                <h2>From Reels to Documentaries</h2>
+              <div
+                className={styles.about__title}>
+                <h2>
+                  From Reels to Documentaries
+                </h2>
               </div>
               <div className={styles.about__text}>
                 <p>
-                Your brand has a story. We help tell that story with creativity so that you stand out in the marketplace. We love discovering your brand’s spark so we can set it on fire.
+                  Your brand has a story. We help
+                  tell that story with creativity
+                  so that you stand out in the
+                  marketplace. We love discovering
+                  your brand’s spark so we can set
+                  it on fire.
                 </p>
               </div>
-              <a href="https://www.youtube.com/channel/UC-fnGeW_PeTre3UzyA9OJqw/" target="_blank" rel="noreferrer" className={styles.vertical__button}>
+              <a
+                href="https://www.youtube.com/channel/UC-fnGeW_PeTre3UzyA9OJqw/"
+                target="_blank"
+                rel="noreferrer"
+                className={
+                  styles.vertical__button
+                }>
                 Our Work
               </a>
             </div>
@@ -105,22 +147,32 @@ export default function Home({ images }) {
                     />
                   ),
                 },
-              ]}
-            ></ParallaxBanner>
+              ]}></ParallaxBanner>
           </ParallaxProvider>
           <Container>
             <div className={styles.about}>
-              <div className={styles.about__title}>
+              <div
+                className={styles.about__title}>
                 <h2>
-                All the <br/>Feels
+                  All the <br />
+                  Feels
                 </h2>
               </div>
               <div className={styles.about__text}>
                 <p>
-                  We understand both the energy of brands and the struggle of trying to communicate that energy with the world. We turn emotion into knowledge and knowledge into movement.
+                  We understand both the energy of
+                  brands and the struggle of
+                  trying to communicate that
+                  energy with the world. We turn
+                  emotion into knowledge and
+                  knowledge into movement.
                 </p>
               </div>
-              <a href="#" className={styles.vertical__button}>
+              <a
+                href="#"
+                className={
+                  styles.vertical__button
+                }>
                 Let's Chat
               </a>
             </div>
@@ -128,5 +180,5 @@ export default function Home({ images }) {
         </div>
       </section>
     </Layout>
-  )
+  );
 }
