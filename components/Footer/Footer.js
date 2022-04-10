3cf5b261f5c828306abadb2cb0938c4f1fc47c34
Trevor <burke.trevor.m@gmail.com>
2022-04-10 12:38:37 -0400

    import next/link on footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+
 import {
   BsYoutube,
   BsInstagram,
@@ -14,9 +16,9 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.footer__item}>
         <div className={styles.contact}>
-          <a href="/contact">
-            <h1>Ready to start?</h1>
-          </a>
+          <h1>
+            <Link href={"/contact"}>Ready to start?</Link>
+          </h1>
         </div>
         <div className={styles.footer__logo}>
           <Image src={logo} width={320} height={70} alt="hexonic main logo" />
